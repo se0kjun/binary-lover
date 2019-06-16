7800ca27991ad36d01e6b6e0d503723f27dd25df
se0kjun <hong921122@gmail.com>
2019-06-16 17:20:05 +0900

    Add default binary viewer, load scripts
    If meta information file doesn't exist, it would be shown a default binary viewer.
    Binary viewer has loaded data in lazy.

--- src/binaryDataLoader.ts
+++ src/binaryDataLoader.ts
@@ -15,11 +15,16 @@ export class BinaryDataLoader {
     private _panel : vscode.WebviewPanel;
     private _activeDoc? : vscode.TextEditor;
     private binaryFileBuffer! : Buffer;
+    private readonly _extensionPath : string;
 
-    public static createBinaryPanel(meta : loader.MetaInfoLoader) {
+    private lazyLoadCnt : number;
+
+    public static createBinaryPanel(extensionPath : string) {
 		const column = vscode.window.activeTextEditor
 			? vscode.window.activeTextEditor.viewColumn
             : undefined;
+        const filePath : vscode.Uri = vscode.Uri.file(path.join(extensionPath, 'src', 'resources'));
+        const meta = new loader.MetaInfoLoader("elf", filePath);
 
         // Otherwise, create a new panel.
 		const panel = vscode.window.createWebviewPanel(
@@ -28,16 +33,20 @@ export class BinaryDataLoader {
 			column || vscode.ViewColumn.One,
 			{
 				// Enable javascript in the webview
-				enableScripts: true,
+                enableScripts: true,
+				// And restrict the webview to only loading content from our extension's `media` directory.
+				localResourceRoots: [vscode.Uri.file(path.join(extensionPath, 'media'))]
 			}
         );
-        
-        BinaryDataLoader.binaryDataLoader = new BinaryDataLoader(panel, meta);
+
+        BinaryDataLoader.binaryDataLoader = new BinaryDataLoader(panel, meta, extensionPath);
     }
 
-    private constructor (panel : vscode.WebviewPanel, meta : loader.MetaInfoLoader) {
+    private constructor (panel : vscode.WebviewPanel, meta : loader.MetaInfoLoader, extPath : string) {
         this.metaInfo = meta;
         this._panel = panel;
+        this._extensionPath = extPath;
+        this.lazyLoadCnt = 0;
 
         bfileLoader.BinaryFileLoader.binaryFileLoad(this.metaInfo);
 
@@ -45,7 +54,7 @@ export class BinaryDataLoader {
 
         this._update();
         this._panel.webview.onDidReceiveMessage(
-            e => this._handleEvent,
+            e => this._handleEvent(e),
             null,
             undefined
         );
@@ -112,10 +121,63 @@ export class BinaryDataLoader {
         return `<html><body>${aa}</body></html>`;
     }
 
+    private _initDefaultViewer() : string {
+        const sizeoOfLazyLoading : any = vscode.workspace.getConfiguration("conf.resource").get("partialUpdateSize");
+        const scriptPath = vscode.Uri.file(
+            path.join(this._extensionPath, 'media', 'main.js'));
+        const scriptUri = scriptPath.with({ scheme: 'vscode-resource' });
+        const nonce = this._getNonce();
+
+        return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src 'nonce-${nonce}';">
+            <title>Binary viewer</title>
+        </head>
+        <body>
+            ${this._lazyLoadHTML(this.lazyLoadCnt, this.lazyLoadCnt += sizeoOfLazyLoading)}
+            <script nonce="${nonce}" src="${scriptUri}"></script>
+        </body>
+        </html>`;
+    }
+
+    private _lazyLoadHTML(start : number, end : number) : string {
+        const numberOfBin : any = vscode.workspace.getConfiguration("conf.resource").get("numberOfBinaryInLine");
+
+        return bfileLoader.BinaryFileLoader.instance.openedFile.slice(start, end)
+            .reduce((data : { output : string, lineBuf : Buffer, bufOffset : number }, val : number, currIdx : number) => {
+                if (currIdx == 0) {
+                    data.output += `<div>0x${(start + currIdx).toString(16).padStart(8, '0').toUpperCase()} || `;
+                }
+                else if (currIdx % numberOfBin == 0) {
+                    // line feed
+                    data.output += `
+                    || ${data.lineBuf.toString('ascii').replace(/[^\x20-\x7E]/g, '.')}</div>
+                    <div>0x${(start + currIdx).toString(16).padStart(8, '0').toUpperCase()} || `;
+                    data.lineBuf = Buffer.allocUnsafe(numberOfBin);
+                    data.bufOffset = 0;
+                }
+
+                data.output += `<span class="test"> ${val.toString(16).padStart(2, '0').toUpperCase()} </span>`;
+                data.lineBuf.writeUInt8(val, data.bufOffset);
+                data.bufOffset++;
+
+                return data;
+            }, { output : "", lineBuf : Buffer.allocUnsafe(numberOfBin), bufOffset : 0}).output;
+    }
+
     private _update() {
         this._panel.webview.html = this._buildHTML();
     }
 
-    private _handleEvent(e : any) {
+    private _getNonce() : string {
+        let text = '';
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for (let i = 0; i < 32; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
     }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -8,29 +8,9 @@ import * as binLoader from './binaryDataLoader';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-// config
-// custom binary format
-// collapse
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "binary-lover" is now active!');
-		
-		// The command has been defined in the package.json file
-		// Now provide the implementation of the command with registerCommand
-		// The commandId parameter must match the command field in package.json
-		let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-			// The code you place here will be executed every time your command is executed
-			
-			
-		let filePath : vscode.Uri = vscode.Uri.file(path.join(context.extensionPath, 'src', 'resources'));
-			
-		let a : loader.MetaInfoLoader = new loader.MetaInfoLoader("elf", filePath);
-		binLoader.BinaryDataLoader.createBinaryPanel(a);
-		// Display a message box to the user
+	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+		binLoader.BinaryDataLoader.createBinaryPanel(context.extensionPath);
 		vscode.window.showInformationMessage('Hello World!');
-
 	});
 
 	context.subscriptions.push(disposable);
