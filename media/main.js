11b27b9869fe22fd816a023862b94beaaedc8470
se0kjun <hong921122@gmail.com>
2019-06-28 01:33:14 +0900

    Can modify continuously binary data

--- media/main.js
+++ media/main.js
@@ -70,4 +70,56 @@
             e.preventDefault();
         }
     });
+
+    document.addEventListener('dblclick', function(mouse) {
+        let mouseTarget = mouse.srcElement;
+        if (mouseTarget.classList.contains('hex_data')) {
+            let value = mouseTarget.innerText;
+            mouseTarget.innerHTML = `<input class="edit-bin" type="text" value="${value.trim()}" maxlength="2">`;
+            focusToEnd(mouseTarget.firstElementChild);
+        }
+    });
+
+    document.addEventListener('keydown', function(e) {
+        if (e.srcElement.classList.contains('edit-bin')) {
+            let value = String(e.srcElement.value);
+            if (e.key == 'Escape') {
+                if (value.length == 2) {
+                    e.srcElement.outerHTML = value.trim();
+                } else if (value.length == 0) {
+                    e.srcElement.parentNode.setAttribute('class', 'hex_data removed_data modified');
+                    e.srcElement.outerHTML = "  ";
+                }
+            }
+            else if (e.key == 'Backspace') {
+                if (value.length == 0) {
+                    let prevSibling = e.srcElement.parentNode.previousElementSibling;
+                    let prevSiblingValue = prevSibling.innerText;
+                    e.srcElement.parentNode.setAttribute('class', 'hex_data removed_data modified');
+                    e.srcElement.outerHTML = "  ";
+                    prevSibling.innerHTML = `<input class="edit-bin" type="text" value="${prevSiblingValue.trim()}" maxlength="2">`;
+                    focusToEnd(prevSibling.firstElementChild);
+                    // TODO: traversing prev sibling when cursor is the first pos in a line
+                }
+            }
+            else if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 65 && e.keyCode <= 90)) {
+                if (value.length == 2) {
+                    let nextSibling = e.srcElement.parentNode.nextElementSibling;
+                    if (nextSibling != null) {
+                        e.srcElement.parentNode.setAttribute('class', 'hex_data modified');
+                        e.srcElement.outerHTML = value.trim();
+                        nextSibling.innerHTML = `<input class="edit-bin" type="text" maxlength="2">`;
+                        focusToEnd(nextSibling.firstElementChild);
+                    }
+                }
+            } else {
+                e.preventDefault();
+            }
+        }
+    });
+
+    function focusToEnd(el) {
+        el.focus();
+        el.setSelectionRange(2, 2);
+    }
 }());
