c6b475134a1d250d4d8ecc4f90e848089a73ad62
se0kjun <hong921122@gmail.com>
2019-06-28 23:46:14 +0900

    Edit sequentially binary data in other line

--- media/main.js
+++ media/main.js
@@ -6,6 +6,7 @@
     let loadLock = false;
     let loadedOffset = document.getElementById('loadOffset').getAttribute('data-load-offset');
     let gotoElem = undefined;
+    let selectedElemValue = 0;
 
     window.addEventListener('scroll', function(ev) {
         if (window.scrollY / (dataContainer.scrollHeight - window.innerHeight) > 0.9
@@ -64,7 +65,7 @@
             var copyElem = selObj.getRangeAt(0).cloneContents();
             var data = "";
             copyElem.querySelectorAll('.hex_data').forEach(value => {
-                data += value.textContent.trimLeft();
+                data += (value.textContent.trim() + " ");
             });
             e.clipboardData.setData('text/plain', data);
             e.preventDefault();
@@ -75,6 +76,7 @@
         let mouseTarget = mouse.srcElement;
         if (mouseTarget.classList.contains('hex_data')) {
             let value = mouseTarget.innerText;
+            selectedElemValue = value;
             mouseTarget.innerHTML = `<input class="edit-bin" type="text" value="${value.trim()}" maxlength="2">`;
             focusToEnd(mouseTarget.firstElementChild);
         }
@@ -83,9 +85,13 @@
     document.addEventListener('keydown', function(e) {
         if (e.srcElement.classList.contains('edit-bin')) {
             let value = String(e.srcElement.value);
-            if (e.key == 'Escape') {
+            if (e.key == 'Escape' || e.key == 'Enter') {
                 if (value.length == 2) {
-                    e.srcElement.outerHTML = value.trim();
+                    if (selectedElemValue != value) {
+                        e.srcElement.parentNode.setAttribute('class', 'hex_data modified');
+                    }
+
+                    e.srcElement.outerHTML = value.trim().toUpperCase();
                 } else if (value.length == 0) {
                     e.srcElement.parentNode.setAttribute('class', 'hex_data removed_data modified');
                     e.srcElement.outerHTML = "  ";
@@ -94,20 +100,45 @@
             else if (e.key == 'Backspace') {
                 if (value.length == 0) {
                     let prevSibling = e.srcElement.parentNode.previousElementSibling;
-                    let prevSiblingValue = prevSibling.innerText;
-                    e.srcElement.parentNode.setAttribute('class', 'hex_data removed_data modified');
-                    e.srcElement.outerHTML = "  ";
-                    prevSibling.innerHTML = `<input class="edit-bin" type="text" value="${prevSiblingValue.trim()}" maxlength="2">`;
-                    focusToEnd(prevSibling.firstElementChild);
-                    // TODO: traversing prev sibling when cursor is the first pos in a line
+                    if (!prevSibling.classList.contains('hex_data')) {
+                        if (prevSibling.parentNode.previousElementSibling != null) {
+                            for (let child of prevSibling.parentNode.previousElementSibling.children) {
+                                if (child.classList.contains('hex_data'))
+                                    prevSibling = child;
+                            }
+                        } else {
+                            prevSibling = null;
+                        }
+                    }
+
+                    if (prevSibling != null) {
+                        let prevSiblingValue = prevSibling.innerText;
+                        e.srcElement.parentNode.setAttribute('class', 'hex_data removed_data modified');
+                        e.srcElement.outerHTML = "  ";
+                        prevSibling.innerHTML = `<input class="edit-bin" type="text" value="${prevSiblingValue.trim()}" maxlength="2">`;
+                        focusToEnd(prevSibling.firstElementChild);
+                    }
                 }
             }
             else if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 65 && e.keyCode <= 90)) {
                 if (value.length == 2) {
                     let nextSibling = e.srcElement.parentNode.nextElementSibling;
+                    if (!nextSibling.classList.contains('hex_data')) {
+                        if (nextSibling.parentNode.nextElementSibling != null) {
+                            for (let child of nextSibling.parentNode.nextElementSibling.children) {
+                                if (child.classList.contains('hex_data')) {
+                                    nextSibling = child;
+                                    break;
+                                }
+                            }
+                        } else {
+                            nextSibling = null;
+                        }
+                    }
+
                     if (nextSibling != null) {
                         e.srcElement.parentNode.setAttribute('class', 'hex_data modified');
-                        e.srcElement.outerHTML = value.trim();
+                        e.srcElement.outerHTML = value.trim().toUpperCase();
                         nextSibling.innerHTML = `<input class="edit-bin" type="text" maxlength="2">`;
                         focusToEnd(nextSibling.firstElementChild);
                     }
